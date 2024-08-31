3b67a93de1d83129c7b1c19a836c13528569652f
Adrian-95 <nedlandftw@gmail.com>
2024-08-31 18:50:26 +0300

    adding menu

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.querySelector('.mobile-menu-toggle');
     const mobileMenu = document.querySelector('.mobile-menu');
+
+    console.log('JavaScript is running'); // Add this line
   
     // Event listener for the toggle button
     toggleButton.addEventListener('click', function() {
